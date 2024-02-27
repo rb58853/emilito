0cc0de1ad1a86b555c1f0bafe39e1aa35e2fc226
rb58853 <rb58853@gmail.com>
2024-02-26 19:03:41 -0500

    save

--- src/components/marketplace/ProductsFromSerie.jsx
+++ src/components/marketplace/ProductsFromSerie.jsx
@@ -2,64 +2,73 @@ import './container.css';
 import ProductMarket from '../products/ProductMarket';
 import "./ProductsFromSerie.css"
 import React, { useState } from 'react';
+import series from "../../data/static_series.js";
+import products from "../../data/static_products.js"
 
-function SerieProducts({items, name}) {
+function SerieProductsContainer({ items, name }) {
   let products = Object.values(items).map(item => {
     return <ProductMarket product={item} />
   })
-  const [showDropdown, setShowDropdown] = useState(false)
-
-
-  return <div>
-    <div className='serie_select_space'>
-      <p className='text_border'>{name}</p>
-      <div className='dropdown'>
-
-        <button className='dropdown_item'
-          onClick={() => {
-            showDropdown ? setShowDropdown(false) : setShowDropdown(true)
-          }}
-        >
-          {name}
-        </button>
-
-        <hr className='line' />
-        <div className={`dropdown_options ${showDropdown ? 'show' : ''}`}>
-          <button className='dropdown_item'
-            onClick={() => {
-              return <p>test</p>
-            }}>
-            serie1
-          </button>
-          <button className='dropdown_item'>
-            serie2
-          </button>
-          <button className='dropdown_item'>
-            serie3
-          </button>
-          <button className='dropdown_item'>
-            serie4
-          </button>
-          <button className='dropdown_item'>
-            serie5
-          </button>
-          <button className='dropdown_item'>
-            serie6
-          </button>
-
-        </div>
-      </div>
-
-    </div>
+  return (
     <div className='container_products_serie'>
       {products}
     </div>
-  </div>;
+  );
 }
 
-// function SerieProducts({ items, name }) {
-//   return (
-//       <SerieProductsContainer items={items} name={name} />
-//   )
-// }
+function SerieProducts({ items, name }) {
+  const [showDropdown, setShowDropdown] = useState(false)
+  const [selfName, setSelfName] = useState(name)
+  const [selfItems, setSelfItems] = useState(items)
+
+  return (
+    <div>
+      <div className='serie_select_space'>
+        <p className='text_border'>{selfName}</p>
+        <div className='dropdown'>
+
+          <button className='dropdown_item'
+            onClick={() => {
+              showDropdown ? setShowDropdown(false) : setShowDropdown(true)
+            }}
+          >
+            {selfName}
+          </button>
+
+          <hr className='line' />
+          <div className={`dropdown_options ${showDropdown ? 'show' : ''}`}>
+          
+            <button className='dropdown_item'
+              onClick={() => {
+                setSelfItems({ 'p1': products['p1'], 'p4': products['p4'] });
+                setSelfName('test')
+              }}
+            >
+              serie1
+            </button>
+            
+            <button className='dropdown_item'>
+              serie2
+            </button>
+            <button className='dropdown_item'>
+              serie3
+            </button>
+            <button className='dropdown_item'>
+              serie4
+            </button>
+            <button className='dropdown_item'>
+              serie5
+            </button>
+            <button className='dropdown_item'>
+              serie6
+            </button>
+
+          </div>
+        </div>
+      </div>
+
+      <SerieProductsContainer items={selfItems} name={selfName} />
+    </div>
+  )
+}
 export default SerieProducts;
