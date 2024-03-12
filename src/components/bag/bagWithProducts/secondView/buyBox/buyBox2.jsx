9a75257131c96642f2d1b9cfa50498b24ae511f0
rb58853 <rb58853@gmail.com>
2024-03-12 13:09:00 -0400

    save

---
+++ src/components/bag/bagWithProducts/secondView/buyBox/buyBox2.jsx
@@ -0,0 +1,53 @@
+import { useSelector } from 'react-redux'
+import StepsBuy from '../../stepsBuy/StepsBuy'
+import './style/desktop.css'
+
+function BuyBox2() {
+    let subtotal = 0
+    const bag = useSelector((state) => state.bag)
+    Object.values(bag.products).forEach(item => {
+        subtotal += parseFloat(item["product"].price) * parseInt(item["count"]);
+    })
+    subtotal = subtotal.toFixed(2)
+
+    return (
+        <div className="buy-box-2">
+            <StepsBuy step={1} />
+            <a className='order-sumary-button'
+                href=''
+            >
+                Order Sumary
+            </a>
+            <div className='notes'>
+                <div>
+                    <text>
+                        Notes or instructions (optional)
+                    </text>
+                    <input className="input-notes"
+                        type="text" />
+                </div>
+            </div>
+            <div className='pays-cost-space'>
+                <div className='row-cost'>
+                    Subtotal:
+                    <text>
+                        {"$" + subtotal}
+                    </text>
+                </div>
+                <div className='row-cost'>
+                    Shipping:
+                    <text>
+                        {"$0"}
+                    </text>
+                </div>
+                <div className='row-cost'>
+                    Tax:
+                    <text>
+                        {"$0"}
+                    </text>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default BuyBox2
--- src/components/bag/bagWithProducts/secondView/view.jsx
+++ src/components/bag/bagWithProducts/secondView/view.jsx
@@ -1,60 +1,14 @@
+import BuyBox2 from "./buyBox/buyBox2";
+import Information from "./information/information";
 import './style/desktop.css'
 
 function BagSecondView() {
     return (
-        <section className='bag-details'>
-            <div className='information'>
-                <div className='email-space'>
-                    <h4 className='header-in-info'>
-                        Email address
-                    </h4>
-                    <input
-                        className='input-info'
-                        type="text"
-                    />
-                </div>
-
-                <div className='shipping-space'>
-                    <h4 className='header-in-info'>
-                        Shipping address
-                    </h4>
-                    <div className='shipping-row'>
-                        <div className='input-space'>
-                            <text>
-                                Country
-                            </text>
-                            <input
-                                className='input-info'
-                                type="text"
-                            />
-                        </div>
-                    </div>
-
-                    <div className='shipping-row'>
-                        <div className='input-space'>
-                            <text>
-                                First name
-                            </text>
-                            <input
-                                className='input-info'
-                                type="text"
-                            />
-                        </div>
-                        <div className='input-space'>
-                            <text>
-                                Last name
-                            </text>
-                            <input
-                                className='input-info'
-                                type="text"
-                            />
-                        </div>
-
-                    </div>
-                </div>
-
-            </div>
-        </section>
+        <div className='bag-details'>
+            <Information/>
+            <BuyBox2/>
+        </div>
     )
 }
+
 export default BagSecondView
