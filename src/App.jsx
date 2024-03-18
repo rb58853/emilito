c63a2ea4365a8f85cdb5a4f81598a9ef59428b0a
rb58853 <rb58853@gmail.com>
2024-03-17 20:02:03 -0400

    scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import FullContainer from './components/fullContainer/FullContainer';
 import TopBars from './components/bars/topBars/TopBars';
 import BottomBars from './components/bars/bottomBars/View/bottomBars.jsx';
 import './components/utils/basic.css'
+import ScrollToTop from './components/utils/scrollToTop.jsx';
 
 function App({ content }) {
   return (
     <div className="App">
-      <TopBars />
-      <FullContainer content={content} />
-      <BottomBars />
+      <ScrollToTop/>
+        <TopBars />
+        <FullContainer content={content} />
+        <BottomBars />
     </div>
   );
 }
